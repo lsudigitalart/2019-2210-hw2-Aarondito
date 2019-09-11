199940d88312738c4bca76ba0b030ea74a1dc934
Aarondito <54543739+Aarondito@users.noreply.github.com>
2019-09-10 21:42:42 -0500

    worked on line work

---
+++ sketch.js
@@ -0,0 +1,38 @@
+function setup() {
+createCanvas(400,269)
+background(200)
+fill(0)
+ellipse(1,1,40,40) 
+ellipse(1,1,20,60)
+ellipse(1,37,20,10)
+strokeWeight(8)
+line(100,1,139,269)
+line(150,1,189,269)
+line(170,135,295,269)
+strokeWeight(10)
+line(250,210,297,269)
+line(1,190,121,190)
+strokeWeight(5)
+line(1,120,113,120)
+strokeWeight(15)
+line(1,260,133,260)
+strokeWeight(12)
+line(350,1,400,90)
+strokeWeight(20)
+line(348,1,370,269)
+strokeWeight(5)
+line(310,1,360,130)
+stroke(300)
+line(325,1,385,145)
+strokeWeight(11)
+line(300,1,400,260)
+line(30,5,93,5)
+strokeWeight(15)
+line(1,133,107,133)
+strokeWeight(5)
+line(1,195,120,195)
+  }
+  
+  function draw() {
+    // Put things you want to over and over here. 
+  }
